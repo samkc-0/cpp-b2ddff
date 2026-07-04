36417608ad334244b4900df2df00b0affc4efa0c
samkc-0 <sam.k.c.0@icloud.com>
2026-07-04 23:25:42 +0100

    do 02-count-words

--- src/stages/stage-01-core-language/01-std-string/02-count-words/exercise.cpp
+++ src/stages/stage-01-core-language/01-std-string/02-count-words/exercise.cpp
@@ -7,8 +7,18 @@ namespace stage_01::std_string::count_words_exercise {
 // Task: Return the number of words in text.
 // Words are separated by one or more whitespace characters.
 std::size_t count_words(std::string_view text) {
-    (void)text;
-    throw std::logic_error("TODO: implement count_words");
+    bool is_space = 1;
+    size_t count = 0;
+    for (char c : text) {
+        bool start = is_space;
+        if (c == ' ' || c == '\t')
+            is_space = 1;
+        else
+            is_space = 0;
+        if (start == 1 && is_space == 0)
+            count += 1;
+    }
+    return count;
 }
 
 }  // namespace stage_01::std_string::count_words_exercise
